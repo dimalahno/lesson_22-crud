7c7128808defc14b285774b412a404c97721219a
dimalahno <dimalahno@rambler.ru>
2025-05-23 16:49:59 +0500

    create app

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'created_at')  # Поля, которые будут отображаться в списке
-    search_fields = ('name', 'description')  # Поля для поиска
-    list_filter = ('created_at',)  # Фильтр по дате создания
+    list_display = ('name', 'price', 'category', 'created_at', 'updated_at',)
+    search_fields = ('name', 'description',)
+    list_filter = ('created_at',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- products/models.py
+++ products/models.py
@@ -1,11 +1,29 @@
 from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название товара")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products", verbose_name="Категория")
     description = models.TextField(verbose_name="Описание товара")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     image = models.ImageField(upload_to="products/", verbose_name="Изображение")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(default=timezone.now, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Продукты"
+        verbose_name_plural = "Продукты"
 
     def __str__(self):
         return self.name
