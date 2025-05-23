823b274243a126dcda2268499ad9b7d638e9d53d
dimalahno <dimalahno@rambler.ru>
2025-05-23 18:33:34 +0500

    add crud for category

--- products/views.py
+++ products/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import CategoryForm
+from .models import Product, Category
 from django.views.generic import ListView, DetailView
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, "products/category_list.html", {"categories": categories})
+
+def category_create(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("products:category_list")
+    else:
+        form = CategoryForm()
+    return render(request, "products/category_form.html", {"form": form})
+
+def category_update(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect("products:category_list")
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, "products/category_form.html", {"form": form})
+
+def category_delete(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect("products:category_list")
 
 
 class ProductListView(ListView):
